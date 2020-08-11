f864c5bcb3ede855b87d83778e563cf8182d1099
gurudattshenoy <66896167+gurudattshenoy@users.noreply.github.com>
2020-08-11 22:17:52 +0530

    Create ex1.py

---
+++ linkedlist/ex1.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        node = Node(data)
+        if self.head == None:
+            self.head = node
+        else:
+            node.next = self.head
+            self.head = node
+
+    def delete(self):
+        if not self.head:
+            print("No nodes")
+        else:
+            temp = self.head
+            self.head = self.head.next
+            return temp.data
+
+    def print(self):
+        temp = self.head
+        while temp:
+            print(temp.data)
+            temp = temp.next
+
